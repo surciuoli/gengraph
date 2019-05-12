85b81605d61403d9b74d1eddb0c122c67e0626aa
surciuoli <surciuoli@gmail.com>
2019-05-12 19:34:37 -0300

    Added option to create undirected graph

--- gengraph.sh
+++ gengraph.sh
@@ -1,18 +1,44 @@
 V=$1
 E=$2
 C=$3
+directed="yes"
+
+for arg in "$@"
+do
+  if [ "$arg" = "-u" ]
+  then 
+    directed="no"
+  fi 
+done
 
 Emax=$((V * (V - 1)))
 
-if [ $E -gt $Emax ]; then
+if [ $E -gt $Emax ]
+then
   E=$Emax
 fi 
 
 function gen_edges () {
-  for (( v=1; v<=$V; v++ )); do
-    for (( w=1; w<=$V; w++ )); do
-      if [ $v -ne $w ]; then
-        echo $v $w $(($RANDOM % $C + 1))
+  for (( v=1; v<=$V; v++ ))
+  do
+  
+    if [ $directed = "yes" ]
+	then 
+	  w0=1
+	else
+	  w0=$((v+1))
+	fi
+	
+    for (( w=$w0; w<=$V; w++ ))
+	do
+      if [ $v -ne $w ]
+	  then
+		if [[ $C =~ ^[0-9]+$ ]]
+		then
+		  echo $v $w $(($RANDOM % $C + 1))
+		else
+		  echo $v $w
+		fi 
       fi
     done 
   done 
